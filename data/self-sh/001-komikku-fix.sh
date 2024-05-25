4ce3ef521a20b7a6c565f07ea2585ef5ad686f63
HDYOU <32186506+HDYOU@users.noreply.github.com>
2024-05-25 16:46:56 +0800

    fix

--- data/self-sh/001-komikku-fix.sh
+++ data/self-sh/001-komikku-fix.sh
@@ -8,7 +8,7 @@ export HOME="$(cd "`dirname "$0"`"/..; pwd)"
 
 echo "test run-self-script ...."
 t_file="app/src/main/java/eu/kanade/tachiyomi/extension/api/ExtensionApi.kt"
-if[ -f $t_file ]; then
+if test -f $t_file ; then
   find_txt="^                getExtensionRepo.getAll()"
   match=`grep "$find_txt" $t_file`
   if [ -n "$match" ]; then
