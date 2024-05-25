340ab4e9b3f37265b9b5597dbe27cd0314244ff8
HDYOU <32186506+HDYOU@users.noreply.github.com>
2024-05-25 23:17:55 +0800

    fix22

--- data/self-sh/004-komikku-fix-serlizable.sh
+++ data/self-sh/004-komikku-fix-serlizable.sh
@@ -6,12 +6,12 @@
 
 export HOME="$(cd "`dirname "$0"`"/..; pwd)"
 
-t_file="domain/src/main/java/tachiyomi/domain/manga/interactor/GetManga.kt"
+t_file="app/src/main/java/eu/kanade/tachiyomi/ui/browse/RelatedMangasScreen.kt"
 if test -f $t_file ; then
-  find_txt=": MetadataSource.GetMangaId"
-  match=`grep "$find_txt" $t_file`
-  if [ -n "$match" ]; then
-    sed -i "s/: MetadataSource.GetMangaId/: MetadataSource.GetMangaId, java.io.Serializable/g" $t_file
+  # find_txt=": MetadataSource.GetMangaId"
+  # match=`grep "$find_txt" $t_file`
+  # if [ -n "$match" ]; then
+    # sed -i "s/: MetadataSource.GetMangaId/: MetadataSource.GetMangaId, java.io.Serializable/g" $t_file
     sed -i "s/    private val getManga:/    @Transient private val getManga:/g" $t_file
     sed -i "s/    private val sourcePreferences:/    @Transient private val sourcePreferences:/g" $t_file
     sed -i "s/    private val libraryPreferences:/    @Transient private val libraryPreferences:/g" $t_file
@@ -21,7 +21,7 @@ if test -f $t_file ; then
     echo "Fix file: $t_file"
     head -n 75 $t_file
     echo ""
-  fi
+  # fi
 fi
 
 
