447c4b9b0d87bc4d074905edc72f2756f89bba50
HDYOU <32186506+HDYOU@users.noreply.github.com>
2024-05-25 16:44:35 +0800

    fix

--- data/self-sh/001-komikku-fix.sh
+++ data/self-sh/001-komikku-fix.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 # 出错退出
+# 出错退出
 # set -e
 
 export HOME="$(cd "`dirname "$0"`"/..; pwd)"
 
 echo "test run-self-script ...."
 t_file="app/src/main/java/eu/kanade/tachiyomi/extension/api/ExtensionApi.kt"
-if[ test -f $t_file ];then
+if[ -f $t_file ]; then
   find_txt="^                getExtensionRepo.getAll()"
   match=`grep "$find_txt" $t_file`
-  if[ -n $match ];then
+  if [ -n "$match" ]; then
     sed -ir "s/^                getExtensionRepo.getAll()/                addAll(getExtensionRepo.getAll()/" app/build.gradle.kts
     sed -i "s/.flatten()/.flatten())/" app/build.gradle.kts
     echo ""
