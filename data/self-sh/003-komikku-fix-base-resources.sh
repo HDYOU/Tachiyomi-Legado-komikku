fb3827b03e482c8bcfc0756ba720a9dafc23d551
HDYOU <32186506+HDYOU@users.noreply.github.com>
2024-05-25 19:24:49 +0800

    fix23

--- data/self-sh/003-komikku-fix-base-resources.sh
+++ data/self-sh/003-komikku-fix-base-resources.sh
@@ -2,33 +2,39 @@
 
 # 出错退出
 # 出错退出
-# set -e
+set -e
 
 export HOME="$(cd "`dirname "$0"`"/..; pwd)"
 
 echo ""
 echo "test replace base resources ...."
 # i18n/src/commonMain/resources/MR/zh-rCN/strings.xml
-fix_txt_list=( '<string name="action_source_search">搜索图源</string>' \
-  '<string name="whats_coming">测试版本-更新日志(快)</string>' \
-  '<string name="update_check_try_now">现在下载</string>' \
-  '<string name="download_cache_renew_interval">下载缓存失效间隔</string>' \
-  '<string name="download_cache_renew_interval_manual">手动</string>' \
-  '<string name="download_cache_renew_interval_1hour">1 小时</string>' \
-  '<string name="download_cache_renew_interval_2hour">2 小时</string>' \
-  '<string name="download_cache_renew_interval_6hour">6 小时</string>' \
-  '<string name="download_cache_renew_interval_12hour">12 小时</string>' \
-  '<string name="download_cache_renew_interval_24hour">1 天</string>' \
-  '<string name="download_cache_renew_interval_info">如果设置为"手动"，则需要使用 "高级"->"无效下载索引"选项操作来刷新每个漫画的失效下载缓存</string>' \
-  '<string name="pref_clean_invalid_downloads">清除无效下载</string>' \
-  '<string name="pref_clean_invalid_downloads_summary">查找并删除书架中未保存的所有下载、文件和文件夹</string>' \
-  '<string name="invalid_downloads_cleaned">已清除无效下载</string>' \
-)
+tmp_file="tmp_myfile.txt"
+cat << EOF > $tmp_file
+<string name="action_source_search">搜索图源</string> 
+<string name="whats_coming">测试版本-更新日志(快)</string> 
+<string name="update_check_try_now">现在下载</string> 
+<string name="download_cache_renew_interval">下载缓存失效间隔</string> 
+<string name="download_cache_renew_interval_manual">手动</string> 
+<string name="download_cache_renew_interval_1hour">1 小时</string> 
+<string name="download_cache_renew_interval_2hour">2 小时</string> 
+<string name="download_cache_renew_interval_6hour">6 小时</string> 
+<string name="download_cache_renew_interval_12hour">12 小时</string> 
+<string name="download_cache_renew_interval_24hour">1 天</string> 
+<string name="download_cache_renew_interval_info">如果设置为"手动"，则需要使用 "高级"->"无效下载索引"选项操作来刷新每个漫画的失效下载缓存</string> 
+<string name="pref_clean_invalid_downloads">清除无效下载</string> 
+<string name="pref_clean_invalid_downloads_summary">查找并删除书架中未保存的所有下载、文件和文件夹</string> 
+<string name="invalid_downloads_cleaned">已清除无效下载</string> 
+EOF
+sed -i '/^$/d' $tmp_file
+sed -i '/^#/d' $tmp_file
+cat $tmp_file
+
 t_file="i18n/src/commonMain/resources/MR/zh-rCN/strings.xml"
 if test -f $t_file ; then
   
 
-  for fix_txt in ${fix_txt_list[*]}; do
+  for fix_txt in `cat $tmp_file`; do
     echo "fix_txt: $fix_txt"
     find_txt=`echo "$fix_txt" | awk -F "\"" "{print $3}"`
     match=`grep "$find_txt" $t_file`
@@ -47,5 +53,5 @@ if test -f $t_file ; then
   tail -n 50 $t_file
   echo ""
 fi
-
+rm -f $tmp_file
 exit 0
